defe53750d560740f2773c90ca7fa8f922c35b1e
Matthijs Mekking <github@pletterpet.nl>
2013-07-26 11:39:24 +0200

    add util functions (hexdigit2int)

--- make_zonec.sh
+++ make_zonec.sh
@@ -1,2 +1,2 @@
-ragel -C -G2 rzonec.rl && gcc rzonec.c dname.c region.c -o zonec
+ragel -C -G2 rzonec.rl && gcc rzonec.c dname.c region.c util.c -o zonec
 
---
+++ util.c
@@ -0,0 +1,70 @@
+/*
+ * $Id: util.c 6747 2012-10-19 10:32:45Z matthijs $
+ *
+ * Copyright (c) 2013 NLNet Labs. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+ * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/**
+ * Utility functions.
+ *
+ */
+
+#include "util.h"
+
+#include <stdio.h>
+
+static const char* logstr = "util";
+
+
+/**
+ * Convert a single (hexidecimal) digit to its integer value.
+ *
+ */
+int
+util_hexdigit2int(char hx)
+{
+    switch (hx) {
+        case '0': return 0;
+        case '1': return 1;
+        case '2': return 2;
+        case '3': return 3;
+        case '4': return 4;
+        case '5': return 5;
+        case '6': return 6;
+        case '7': return 7;
+        case '8': return 8;
+        case '9': return 9;
+        case 'a': case 'A': return 10;
+        case 'b': case 'B': return 11;
+        case 'c': case 'C': return 12;
+        case 'd': case 'D': return 13;
+        case 'e': case 'E': return 14;
+        case 'f': case 'F': return 15;
+        default:
+            fprintf(stderr, "[%s] invalid hex char %c\n", logstr, hx);
+    }
+    return -1;
+}
+
