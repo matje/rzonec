7ba8d6d42999f30de89e51e18e9f7514095671a0
Matthijs Mekking <github@pletterpet.nl>
2013-07-27 22:38:14 +0200

    and add the file zonec.h

---
+++ zonec.h
@@ -0,0 +1,14 @@
+/*
+ * zonec.h -- zone compiler functions.
+ *
+ * Copyright (c) 2013, Matthijs Mekking, NLnet Labs. All rights reserved.
+ *
+ * See LICENSE for the license.
+ *
+ */
+
+#ifndef _NLNETLABS_UTIL_ZONEC_H_
+#define _NLNETLABS_UTIL_ZONEC_H_
+
+#endif /* _NLNETLABS_UTIL_ZONEC_H_ */
+
